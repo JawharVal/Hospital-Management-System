3b4d129b3998ffc4187bb10a44b89c1ba987a4de
Jawhar Maatouk <112645981+JawharVal@users.noreply.github.com>
2023-11-21 13:46:25 +0300

    Delete src/main/java/org/example directory

--- src/main/java/org/example/DI/Component.java
+++
@@ -1,9 +0,0 @@
-package org.example.DI;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Component {
-
-}
--- src/main/java/org/example/DI/Inject.java
+++
@@ -1,8 +0,0 @@
-package org.example.DI;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.LOCAL_VARIABLE, ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Inject {
-}
